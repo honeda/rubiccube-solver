37816a9d1d67f3467987f2769be0763c62015656
honeda <50893852+honeda@users.noreply.github.com>
2023-07-01 22:45:09 +0900

    feat: new network

--- src/neural_network/network.py
+++ src/neural_network/network.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 from src.env.action import ACTION_NUMS, SURFACE_CHARS
@@ -5,61 +6,80 @@ from src.env.action import ACTION_NUMS, SURFACE_CHARS
 N_CHANNEL = len(SURFACE_CHARS)
 
 
+class ResNetBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResNetBlock, self).__init__()
+
+        self.relu = nn.ReLU()
+
+        # 畳み込み層の後ろがBatchNormalization層の場合はbiasは省略.
+        # ブロックの位置が重要なためダウンサンプリング(MaxPool)は行わない.
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out = self.relu(out + x)
+
+        return out
+
+
 class Model(nn.Module):
     """
-    bn: False
+    bn: True
     dropout: False
-    resnet: False
+    resnet: True
 
     * Policyの出力層について
      今回のモデルでは正解ラベルが2つ以上ある(最短手数のアクションが2つ以上ある)
      場合があるのでSoftmax + Crossentropy ではなく、
      Sigmoid + BinaryCrossentropy を使う.(多ラベル分類)
     """
-    def __init__(self):
+    def __init__(self, channels=N_CHANNEL * 2, blocks=3):
         super(Model, self).__init__()
 
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
-        # ブロックの位置が重要なためダウンサンプリング(MaxPool)は行わない.
-        self.conv1 = nn.Conv2d(in_channels=N_CHANNEL, out_channels=N_CHANNEL * 2,
-                               kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=N_CHANNEL * 2, out_channels=N_CHANNEL * 2,
-                               kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=N_CHANNEL * 2, out_channels=N_CHANNEL,
-                               kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=N_CHANNEL, out_channels=channels,
+                               kernel_size=3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(N_CHANNEL)
 
-        self.flatten = nn.Flatten()
+        self.blocks = nn.Sequential(*[ResNetBlock(channels) for _ in range(blocks)])
 
         # policy
-        self.policy_fn1 = nn.Linear(N_CHANNEL * 3 * 3, 28)
-        self.policy_fn2 = nn.Linear(28, len(ACTION_NUMS))
+        self.policy_conv = nn.Conv2d(in_channels=channels, out_channels=len(ACTION_NUMS))
+        self.policy_bn = nn.BatchNorm2d(len(ACTION_NUMS))
+        self.policy_fc1 = nn.Linear(len(ACTION_NUMS) * 3 * 3, 32)
+        self.policy_fc2 = nn.Linear(32, len(ACTION_NUMS))
 
         # value
-        self.value_fn1 = nn.Linear(N_CHANNEL * 3 * 3, 28)
-        self.value_fn2 = nn.Linear(28, 1)
+        self.value_conv = nn.Conv2d(in_channels=channels, out_channels=N_CHANNEL)
+        self.value_bn = nn.BatchNorm2d(N_CHANNEL)
+        self.value_fc1 = nn.Linear(N_CHANNEL * 3 * 3, 32)
+        self.value_fc2 = nn.Linear(32, 1)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
         x = self.relu(x)
 
-        x = self.conv2(x)
-        x = self.relu(x)
+        # resnet blocks
+        x = self.blocks(x)
 
-        x = self.conv3(x)
-        x = self.relu(x)
+        policy = self.relu(self.policy_bn(self.policy_conv(x)))
+        policy = self.relu(self.policy_fc1(torch.flatten(policy, 1)))
+        policy = self.sigmoid(self.policy_fc2(policy))
 
-        x = self.flatten(x)
-
-        policy = self.policy_fn1(x)
-        policy = self.relu(policy)
-        policy = self.policy_fn2(policy)
-        policy = self.sigmoid(policy)
-
-        value = self.value_fn1(x)
-        value = self.relu(value)
-        value = self.value_fn2(value)
-        value = self.sigmoid(value)
+        value = self.relu(self.value_bn(self.value_conv(x)))
+        value = self.relu(self.value_fc1(torch.flatten(value, 1)))
+        value = self.sigmoid(self.value_fc2(value))
 
         return policy, value
